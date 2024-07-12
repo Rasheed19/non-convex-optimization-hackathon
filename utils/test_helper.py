2e955ff8e0c88461be6d348ae5a284c90073e22d
Rasheed19 <ibraheem.abdulrasheed@gmail.com>
2024-07-12 10:58:08 +0100

    fix model load issue

--- utils/test_helper.py
+++ utils/test_helper.py
@@ -2,22 +2,29 @@ import random
 import torch
 import numpy as np
 
+
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+
 def score(accu):
-    return 100*sigmoid(0.1*(accu-50)) + 100*(1-sigmoid(5))
+    return 100 * sigmoid(0.1 * (accu - 50)) + 100 * (1 - sigmoid(5))
+
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
+
 def load_checkpoint(filepath, device):
-    checkpoint = torch.load(filepath)
-    model = checkpoint['model']
-    model.load_state_dict(checkpoint['model_state_dict'])
+    # checkpoint = torch.load(filepath)
+    # model = checkpoint['model']
+    # model.load_state_dict(checkpoint['model_state_dict'])
+    model = torch.Module()
+    model.load_state_dict(torch.load(filepath))
     model = model.to(device)
 
     return model
+
 
 def reset(seed):
     random.seed(seed)
